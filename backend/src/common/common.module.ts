f9262534bc1af4acc1449db088e2d7e790478c7e
Joshua-Tihabwangye <tihabwangyej@gmail.com>
2026-05-09 21:48:26 +0300

    I have added backend integration gateway

--- backend/src/common/common.module.ts
+++
@@ -1,9 +0,0 @@
-import { Global, Module } from '@nestjs/common';
-import { ApiResponseService } from './api/api-response.service';
-
-@Global()
-@Module({
-  providers: [ApiResponseService],
-  exports: [ApiResponseService],
-})
-export class CommonModule {}
--- backend/src/storage/storage.module.ts
+++
@@ -1,9 +0,0 @@
-import { Global, Module } from '@nestjs/common';
-import { InMemoryStoreService } from './in-memory-store.service';
-
-@Global()
-@Module({
-  providers: [InMemoryStoreService],
-  exports: [InMemoryStoreService],
-})
-export class StorageModule {}
